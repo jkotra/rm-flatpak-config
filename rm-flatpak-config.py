4e773ffb9fc1da9380f3bf4a363e90b13c13c249
Jagadeesh Kotra <jagadeesh@stdin.top>
2020-10-01 16:56:05 +0530

    initial release

--- rm-flatpak-config.py
+++ rm-flatpak-config.py
@@ -1,20 +1,71 @@
+#!/usr/bin/env python
+
 import os
 import tarfile
 import time
 from typing import List
+import argparse
+import shutil
+import subprocess
 
-FLATPAK_SYSTEM_CONFIG_PATH = ""
-FLATPAK_USER_CONFIG_PATH = ""
+parser = argparse.ArgumentParser(
+    description='rm-flatpak-config - Remove config files of flatpak applications!')
 
-def list_configs() -> List[str]:
-    user_configs = [path for path in os.listdir(FLATPAK_USER_CONFIG_PATH)]
-    return user_configs
+parser.add_argument('--debug','-D', action='store_true', dest="debug")
+parser.add_argument('--version','-v', action='store_true', dest="version")
 
-def delete() -> bool:
-    pass
+args = parser.parse_args()
 
-def backup() -> bool:
-    pass
+FLATPAK_SYSTEM_CONFIG_PATH = "/var/lib/flatpak/app"
+FLATPAK_USER_CONFIG_PATH = os.path.expanduser("~") + "/.var/app/"
+
+def show_version():
+    print("rm-flatpak-config v0.1")
+    exit(0)
+
+def list_configs() -> List:
+    config_files = []
+    config_files = [(FLATPAK_SYSTEM_CONFIG_PATH, path, "system")
+                    for path in os.listdir(FLATPAK_SYSTEM_CONFIG_PATH)]
+    config_files += [(FLATPAK_USER_CONFIG_PATH, path, "user")
+                     for path in os.listdir(FLATPAK_USER_CONFIG_PATH)]
+
+    return config_files
+
+
+def delete(config_dir) -> bool:
+    if args.debug:
+        print("Deleting", config_dir)
+
+    if FLATPAK_SYSTEM_CONFIG_PATH in config_dir:
+        subprocess.run(["sudo", "rm", "-rf", config_dir])
+    else:
+        shutil.rmtree(config_dir)
+
 
 if __name__ == "__main__":
-    pass
+
+    if args.version:
+        show_version()
+
+    conf_files = list_configs()
+
+    index = 0
+
+    for _, d, t in conf_files:
+        print(index, d, t)
+        index += 1
+    prompt = "\nChoose [0-{}]:".format(len(conf_files))
+    user_choice = input(prompt)
+    if args.debug:
+        print("user_choice=", user_choice)
+
+    try:
+        user_choice = conf_files[int(user_choice)]
+        if args.debug:
+            print("user_choice=", user_choice)
+    except IndexError:
+        print("no such thing exists!")
+        exit(1)
+
+    delete(os.path.join(user_choice[0], user_choice[1]))
